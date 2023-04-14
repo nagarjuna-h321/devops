ed273ce831dd0fdedded096f78a210010dab6e56
nagarjuna <nagarjunahanchate321@gmail.com>
2023-04-14 13:25:47 +0530

    585

---
+++ aws/src/main/java/devops/Anaga.java
@@ -0,0 +1,13 @@
+package devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class Anaga
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
